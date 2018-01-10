37120cd843f953d135e2a37bd6fcd409c6050713
gavinpham <gpham@smu.edu>
2018-01-09 19:31:43 -0800

    Modified footer to include links to linkedin and github. Added minor stylings to allow content to fill page.

--- assets/js/hamburger.js
+++ assets/js/hamburger.js
@@ -1,31 +1,36 @@
 var hamburger = document.querySelector(".hamburger");
 var nav = document.querySelector(".navigation-list");
 var nav_options = document.querySelectorAll(".nav-option");
+var nav_options_length = nav_options.length;
 
 var animation_in = "animation-expand-menu";
 var animation_out = "animation-collapse-menu";
 var animation_appear = "animation-appear";
 var animation_disappear = "animation-disappear";
 var delay = 100;	//Units: ms
-var total_delay = nav_options.length * delay;
+var total_delay = nav_options_length * delay;
 
 function toggleNavigationVisible(){
 	if (nav.classList.contains(animation_in)){
-		for (var i = nav_options.length - 1; i >= 0; i--){
+		//	Animate list elements to disappear, starting from the last element
+		for (var i = nav_options_length - 1; i >= 0; i--){
 			setTimeout((i) => {
 				nav_options[i].classList.replace(animation_appear, animation_disappear);
-			}, (nav_options.length - 1 - i) * delay, i);
+			}, (nav_options_length - 1 - i) * delay, i);
 		}
 		nav.classList.replace(animation_in, animation_out);
 	} else if (nav.classList.contains(animation_out)) {	
-		for (var i = 0; i < nav_options.length; i++){
+		//	Animate list elements to appear, starting from the first element
+		for (var i = 0; i < nav_options_length; i++){
 			setTimeout((i) => {
 				nav_options[i].classList.replace(animation_disappear, animation_appear);
 			}, i * delay, i);
 		}
 		nav.classList.replace(animation_out, animation_in);
 	} else {
-		for (var i = 0; i < nav_options.length; i++){
+		//	Animate list elements to appear, starting from the first element.
+		//	This call is executed only on the first click to the hamburger.
+		for (var i = 0; i < nav_options_length; i++){
 			setTimeout((i)=>{
 				console.log(i);
 				nav_options[i].classList.add(animation_appear);
