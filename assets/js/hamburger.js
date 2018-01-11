31b5112c8f01de631f0e3e96fb818ff126b312d5
gavinpham <gpham@smu.edu>
2018-01-10 21:30:33 -0800

    Minor QOL changes to timing and hover animations

--- assets/js/hamburger.js
+++ assets/js/hamburger.js
@@ -3,30 +3,30 @@ var nav = document.querySelector(".navigation-list");
 var nav_options = document.querySelectorAll(".nav-option");
 var nav_options_length = nav_options.length;
 
-var animation_in = "animation-expand-menu";
-var animation_out = "animation-collapse-menu";
+var animation_expand_menu = "animation-expand-menu";
+var animation_collapse_menu = "animation-collapse-menu";
 var animation_appear = "animation-appear";
 var animation_disappear = "animation-disappear";
-var delay = 100;	//Units: ms
+var delay = 120;	//Units: ms
 var total_delay = nav_options_length * delay;
 
 function toggleNavigationVisible(){
-	if (nav.classList.contains(animation_in)){
+	if (nav.classList.contains(animation_expand_menu)){
 		//	Animate list elements to disappear, starting from the last element
 		for (var i = nav_options_length - 1; i >= 0; i--){
 			setTimeout((i) => {
 				nav_options[i].classList.replace(animation_appear, animation_disappear);
 			}, (nav_options_length - 1 - i) * delay, i);
 		}
-		nav.classList.replace(animation_in, animation_out);
-	} else if (nav.classList.contains(animation_out)) {	
+		nav.classList.replace(animation_expand_menu, animation_collapse_menu);
+	} else if (nav.classList.contains(animation_collapse_menu)) {
 		//	Animate list elements to appear, starting from the first element
 		for (var i = 0; i < nav_options_length; i++){
 			setTimeout((i) => {
 				nav_options[i].classList.replace(animation_disappear, animation_appear);
-			}, i * delay, i);
+			}, i * delay + 100, i);
 		}
-		nav.classList.replace(animation_out, animation_in);
+		nav.classList.replace(animation_collapse_menu, animation_expand_menu);
 	} else {
 		//	Animate list elements to appear, starting from the first element.
 		//	This call is executed only on the first click to the hamburger.
@@ -34,9 +34,9 @@ function toggleNavigationVisible(){
 			setTimeout((i)=>{
 				console.log(i);
 				nav_options[i].classList.add(animation_appear);
-			}, i * delay, i);
+			}, i * delay + 100, i);
 		}
-		nav.classList.add(animation_in);
+		nav.classList.add(animation_expand_menu);
 	}
 }
 
